4e432ad851f52a571a5428febe63d73cc25411b6
Nitesh Singh <niteshsingh7740@gmail.com>
2025-11-11 14:14:31 +0530

    Ready for deployment

---
+++ backend/src/app.js
@@ -0,0 +1,29 @@
+import express from "express";
+ import {createServer} from "node:http";
+
+//  import  Server  from "socket.io";
+
+import mongoose from "mongoose";
+import cors from "cors";
+import userRoutes from "./routes/users.route.js"
+import  {connectToSocket}  from "./controllers/socketManager.js";
+
+const app=express();
+const server=createServer(app);
+const io=connectToSocket(server);
+
+app.set("port",(process.env.PORT || 8000));
+app.use(cors());
+app.use(express.json({limit:"40kb"}));
+app.use(express.urlencoded({limit:"40kb",extended:true}));
+
+app.use("/api/v1/users",userRoutes);
+let start=async()=>{
+    app.set("mongo_user");
+    const connectiondb=await mongoose.connect("mongodb+srv://Easycall_user:Easycall770@easycall.pfrzjeo.mongodb.net/?appName=Easycall");
+    console.log(`MONGO Connected DB Host:${connectiondb.connection.host}`);
+server.listen(app.get("port"),()=>{
+    console.log("LISTENING PORT 8000");
+})
+}
+start();
